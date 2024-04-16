e0861dd07cfe4d8d13d6e8868d0085eb0845204f
Guillaume Debas <guillaumedebas@hotmail.fr>
2024-04-16 16:30:50 +0200

    database connection established

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://btbuser:44pgy5YeT4R3kL@cluster0.odxgzps.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use(express.json());
 
